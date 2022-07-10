c29e1e54e64e4769a9104cf9527f6436db008a63
Isam03 <nfs.issamhajji@gmail.com>
2022-07-10 16:08:57 +0200

    ChatMessage
    Creacion modelo ChatMessage

--- src/main/java/com/alexisam/chatapp/model/ChatMessage.java
+++ src/main/java/com/alexisam/chatapp/model/ChatMessage.java
@@ -1,4 +1,39 @@
 package com.alexisam.chatapp.model;
 
 public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    public MessageType getType(){
+        return type;
+    }
+
+    public void setType(MessageType x){
+        this.type = x;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public void setContent(String x) {
+        this.content = x;
+    }
+
+    public String getSender(){
+        return sender;
+    }
+
+    public void setSender(String x) {
+        this.sender = x;
+    }
+
 }
