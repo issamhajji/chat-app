57fec6f6d8d9134195aca56f72d34869f983ede5
Alex-9aa <109233433+Alex-9aa@users.noreply.github.com>
2022-07-14 18:22:32 +0200

    Update WebSocketListeners.java

--- src/main/java/com/alexisam/chatapp/controller/WebSocketListeners.java
+++ src/main/java/com/alexisam/chatapp/controller/WebSocketListeners.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 
+@Component
 public class WebSocketListeners {
 
     private static final Logger logger = LoggerFactory.getLogger(WebSocketListeners.class); // Imprime registros en la consola
